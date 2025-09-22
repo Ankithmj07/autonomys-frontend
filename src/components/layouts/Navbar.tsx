cc92f4251fec1d1703e78413c78ffce99529adb6
Ankithmj07 <mjankith007@gmail.com>
2025-09-22 22:50:03 +0530

    RFesponsive Changes

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -10,15 +10,16 @@ const Navbar: React.FC = () => {
   const menuItems = ['Protocol', 'Products', 'Ecosystem', 'Community', 'Blog', 'About'];
 
   return (
-    <nav className="fixed sticky z-1000 w-full py-3 flex justify-between px-4 space-x-28 xl:px-35 2xl:px-35 md:space-x-138 lg:space-x-30 xl:space-x-38 items-center mt-5">
+    
+    <nav className="fixed sticky z-1000 w-full py-3 flex justify-between px-4 space-x-28 lg:px-24  xl:px-35 md:space-x-138 lg:space-x-30 xl:space-x-38 items-center mt-5">
       {/* Logo */}
       <div className='w-[160px] h-[52px] px-[22px] pt-4 pt-[18px] bg-[#292a4100] backdrop-blur-lg rounded-[14px]'>
         <img src={autonomysLogo} alt="Autonomys Logo" className="w-[116px]" />
       </div>
 
       {/* Desktop Menu */}
-      <div className="hidden lg:flex space-x-8 px-5 text-white font-medium bg-[#292a4126] backdrop-blur-sm rounded-[14px] h-[52px]">
-        <ul className="hidden lg:flex space-x-10 text-white font-medium">
+      <div className="hidden lg:flex space-x-8  px-5 text-white font-medium bg-[#292a4126] backdrop-blur-sm rounded-[14px] h-[52px]">
+        <ul className="hidden lg:flex space-x-10  2xl:space-x-17 text-white font-medium">
           {menuItems.map((item) => (
             <li key={item} className="cursor-pointer hover:opacity-80 flex items-center gap-1">
               {item}
